33ab9fc0fde4e8622c24ea91d559ece80e2175a2
nera1981 <nermin.vucinic@hotmail.com>
2024-04-17 17:49:27 +0200

    Add Backend Scaffold

---
+++ src/main/java/com/neron/krello/KrelloApplication.java
@@ -0,0 +1,13 @@
+package com.neron.krello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrelloApplication.class, args);
+	}
+
+}
